8c17133a31451bc49f73abbc2d05ac35c1c36497
kennethbigler <kennethbigler@gmail.com>
2015-05-19 16:45:30 -0700

    finished updating content, added favicon, added g analytics, and did much more

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,5 +1,13 @@
 // JavaScript Document
 
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-54229318-1', 'auto');
+ga('send', 'pageview');
+
 /* This is code to load in the Current NetApp stock price
  * I am using the Yahoo! Stock API: YQL
  * To change the stock that loads, change the symbol variable
