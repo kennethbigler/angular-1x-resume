34eec3d40ab45e11ccbaf0bb5b65c62a58b7d431
Kenneth Bigler <kennethbigler@gmail.com>
2015-09-18 08:25:16 -0700

    updated colors and fixed small bugs

--- js/directives/lhs.js
+++ js/directives/lhs.js
@@ -2,9 +2,9 @@ app.directive('lhs', function() {
   return {
     restrict: 'E',
     templateUrl: 'js/directives/lhs.html',
-    controller: ['$scope', 'quote', function($scope, quote) {
+    controller: function($scope, quote) {
 		$scope.tsla = quote.getQ('tsla');
 		$scope.ntap = quote.getQ('ntap');
-	}]
+	}
   };
 });
