121e1c26136ccd868f2fe9cc602b131e44ada155
kennethbigler <kennethbigler@gmail.com>
2017-02-18 11:44:41 -0800

    updating data and graphs

--- app/directives/VisualizationDirective.js
+++ app/directives/VisualizationDirective.js
@@ -1,30 +1,54 @@
 /*global app*/
+// ---------------------------------------------------------------------------
+// Directive used to remove undefined errors in svg elements
+// ---------------------------------------------------------------------------
 app.directive('ngXa', function () {
-    "use strict";
+    'use strict';
     return function (scope, element, attrs) {
         attrs.$observe('ngXa', function (value) {
             attrs.$set('x1', value);
         });
     };
 }).directive('ngXb', function () {
-    "use strict";
+    'use strict';
     return function (scope, element, attrs) {
         attrs.$observe('ngXb', function (value) {
             attrs.$set('x2', value);
         });
     };
 }).directive('ngYa', function () {
-    "use strict";
+    'use strict';
     return function (scope, element, attrs) {
         attrs.$observe('ngYa', function (value) {
             attrs.$set('y1', value);
         });
     };
 }).directive('ngYb', function () {
-    "use strict";
+    'use strict';
     return function (scope, element, attrs) {
         attrs.$observe('ngYb', function (value) {
             attrs.$set('y2', value);
+        });
+    };
+}).directive('ngX', function () {
+    'use strict';
+    return function (scope, element, attrs) {
+        attrs.$observe('ngX', function (value) {
+            attrs.$set('x', value);
+        });
+    };
+}).directive('ngY', function () {
+    'use strict';
+    return function (scope, element, attrs) {
+        attrs.$observe('ngY', function (value) {
+            attrs.$set('y', value);
+        });
+    };
+}).directive('ngWidth', function () {
+    'use strict';
+    return function (scope, element, attrs) {
+        attrs.$observe('ngWidth', function (value) {
+            attrs.$set('width', value);
         });
     };
 });
