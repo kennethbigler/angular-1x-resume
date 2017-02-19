7381d9442227bcdac8b1417e3ca8d398e4f44a14
kennethbigler <kennethbigler@gmail.com>
2017-02-18 18:57:03 -0800

    loading dynamically

--- assets/js/includes.js
+++ assets/js/includes.js
@@ -1,5 +1,10 @@
+// DO NOT USE 'use strict';
 // a list of all the scripts I am using in my homepage
 var includes = [
+    // jQuery
+    'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js',
+    // Bootstrap
+    'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js',
     // angular
     'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.10/angular.js',
     'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.10/angular-route.min.js',
@@ -9,17 +14,13 @@ var includes = [
     'app/controllers/HomeController.js',
     'app/directives/lhs.js',
     'app/directives/VisualizationDirective.js',
-    // jQuery
-    'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js',
-    // Bootstrap
-    'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js',
     // Google Analytics
     'assets/js/analytics.js'
 ];
 
 // iterate through and create js tags for each
 includes.forEach(function (src) {
-    "use strict";
+    // "use strict";
     var script = document.createElement('script');
     script.type = "text/javascript";
     script.src = src;
